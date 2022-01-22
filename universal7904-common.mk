adf143cbd33e3102425a9c81624c567f4140b730
Joker-V2 <nipinna0@gmail.com>
2022-01-23 01:23:18 +0800

    universal7904: Add GoogleCameraGo
    * Don't build Camera2
    
    Signed-off-by: eun0115 <gianpaoloestacio5@gmail.com>

--- universal7904-common.mk
+++ universal7904-common.mk
@@ -41,7 +41,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.5-service_64.universal7904
+    android.hardware.camera.provider@2.5-service_64.universal7904 \
+    GoogleCameraGo
 
 # ConfigStore
 PRODUCT_PACKAGES += \
