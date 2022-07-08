6b46ed8abdc074d90a317a7c3072a65d1fc92426
eun0115 <gianpaoloestacio5@gmail.com>
2022-07-08 18:06:14 +0800

    universal7904: Remove unwanted gapps
    Signed-off-by: eun0115 <gianpaoloestacio5@gmail.com>

--- universal7904-common.mk
+++ universal7904-common.mk
@@ -47,6 +47,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     disable_configstore
 
+# Debloat
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
