4b175ce94c08ba8183742966403481111ceac0cb
eun0115 <gianpaoloestacio5@gmail.com>
2022-07-25 12:01:10 +0800

    [WIP] universal7904: Introducing Samsung Parts
    taken from https://github.com/KharaMe-Devices/android_device_realme_sdm710-common/commit/51f632fa5b48c600d457b9407b27602f1c9097a1
    
    Signed-off-by: MadhavSaladi <baaswanthmadhav@gmail.com>
    Signed-off-by: eun0115 <gianpaoloestacio5@gmail.com>

--- universal7904-common.mk
+++ universal7904-common.mk
@@ -149,6 +149,10 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 # RRO (Runtime Resource Overlay)
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
+# Samsung Parts
+PRODUCT_PACKAGES += \
+    SamsungParts
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
